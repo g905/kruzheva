109a903cc51e7db1690c3403ecdf438557e3c233
g905 <breakestra@yandex.ru>
2019-08-29 13:30:10 +0400

    init commit

---
+++ src/js/index.js
@@ -0,0 +1,81 @@
+import $ from 'jquery';
+import 'popper.js';
+import 'bootstrap';
+import AOS from 'aos'
+import 'slick-carousel'
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { faViber, faSkype, faWhatsapp, faVk, faYoutube, faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons'
+import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
+library.add( faViber, faSkype, faWhatsapp, faVk, faYoutube, faFacebook, faInstagram, faMapMarkerAlt );
+
+// ====================================== Replace fa-icons with SVGs ===============================
+dom.watch();
+
+$(()=>{
+    // ====================================== Animate on scroll ====================================
+    AOS.init();
+
+    // ====================================== Loader ===============================================
+
+    $('.loader-area').fadeOut().end().delay(400).fadeOut('slow');
+
+    // ====================================== Show phone by click ==================================
+
+    $('.sh').click((e)=>{
+        let phone = $(e.target).data('phone');
+        if(!$(e.target).hasClass('clicked')){
+            $(e.target).addClass('clicked');
+            $(e.target).text(phone);
+        } else {
+            location.href = 'tel:'+phone;
+        }
+
+    });
+
+    // ====================================== Test pug and js ======================================
+
+    let template = require("../pug/users.pug");
+    let locals = {
+        users: [
+            "user1",
+            "user2",
+            "user3"
+        ]
+    };
+
+    $('.test').html(template(locals));
+
+    // ====================================== Slick sliders ========================================
+
+    let slick1 = {
+        "infinite": true,
+        "arrows": false,
+        "dots": true,
+        "autoplay": true,
+        "slidesToShow": 1
+    };
+
+    let slick4 = {
+        "infinite": false,
+        "arrows": false,
+        "dots": true,
+        "autoplay": false,
+        "slidesToShow": 4,
+        responsive: [{
+            "breakpoint": "960",
+            "settings": {
+                "slidesToShow": 1
+            }
+        }
+        ]
+    };
+
+    $('.slick1').each((idx, el)=>{
+        $(el).slick(slick1);
+    });
+
+    $('.slick4').each((idx, el)=>{
+        $(el).slick(slick4);
+    })
+
+});
---
+++ webpack.config.js
@@ -0,0 +1,114 @@
+const path = require('path');
+
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const CopyWebpackPlugin= require('copy-webpack-plugin');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+    optimization: {
+      minimizer: [
+          new UglifyJsPlugin({
+              cache: true,
+              parallel: true,
+              sourceMap: true
+          }),
+          new OptimizeCSSAssetsPlugin({})
+      ]
+    },
+    entry: [
+        './src/js/index.js',
+        './src/sass/style.sass'
+    ],
+    output: {
+        filename: './js/bundle.js'
+    },
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'src/js'),
+                exclude: "/node_modules/",
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            },
+            {
+                test: /\.(sass|scss)$/,
+                include: path.resolve(__dirname, 'src/sass'),
+                use: ExtractTextPlugin.extract({
+                    use: [{
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true,
+                            url: false
+                        },
+                    },
+                        {
+                        loader: "postcss-loader",
+                        options: {
+                            plugins: [
+                                autoprefixer({
+                                    overrideBrowserslist: ['ie >= 8', 'last 4 versions']
+                                })
+                            ],
+                            sourceMap: true
+                        }
+
+                    },
+                        {
+                            loader: "sass-loader",
+                            options: {
+                                sourceMap: true,
+
+                            }
+                        }
+                    ]
+                })
+            },
+            {
+                test: /\.pug$/,
+                use: [{
+                    loader: 'pug-loader',
+                    options: {
+                        pretty: true
+                    }
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: './css/style.bundle.css',
+            allChunks: true
+        }),
+        new CopyWebpackPlugin([{
+            from: './src/fonts',
+            to: './fonts'
+        },
+            {
+                from: './src/favicon',
+                to: './favicon'
+            },
+            {
+                from: './src/img',
+                to: './img'
+            }
+        ]),
+        new ImageminPlugin({
+            pngquant: {
+                quality: '50-50'
+            }
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/pug/index.pug'
+        }),
+    ]
+};
